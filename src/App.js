9acd1281ca10071419087c2d067e41b7707ee0f0
cptvictor95 <cpt.victor@hotmail.com>
2021-03-01 15:14:24 -0300

    home page finished

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, CSSReset } from "@chakra-ui/react";
 import Router from "./routing/Router";
 import React from "react";
+import futureEatsTheme from "./futureEatsTheme";
 
 function App() {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={futureEatsTheme}>
+      <CSSReset />
       <Router />
     </ChakraProvider>
   );
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,38 @@
-import { Flex, Heading } from "@chakra-ui/react";
+import { Box, Flex, Text } from "@chakra-ui/react";
 import React from "react";
 
 const Home = () => {
   return (
     <Flex as="main" w="100vw" h="100vh" direction="column" align="center">
-      <Flex as="article" w="100%" h="100%" align="center" justify="center">
-        <Heading as="h1">hello world!</Heading>
+      <Flex
+        as="article"
+        w="100%"
+        h="100%"
+        direction="column"
+        align="center"
+        justify="center"
+        bgColor="neutralPalette.900"
+      >
+        <Box as="span">
+          <Text
+            fontSize="56px"
+            fontWeight="400"
+            lineHeight="0.9"
+            alignSelf="flex-end"
+            color="neutralPalette.100"
+          >
+            Future
+          </Text>
+          <Text
+            fontSize="56px"
+            fontWeight="700"
+            lineHeight="0.9"
+            alignSelf="flex-end"
+            color="greenPalette.400"
+          >
+            Eats
+          </Text>
+        </Box>
       </Flex>
     </Flex>
   );
