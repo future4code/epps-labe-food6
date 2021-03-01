9d0bef073cff2af9da84e90c55eac7518d040536
cptvictor95 <cpt.victor@hotmail.com>
2021-03-01 20:18:20 -0300

    signUp validation started, request made with toasts

--- src/routing/Router.js
+++ src/routing/Router.js
@@ -3,22 +3,25 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
 import SignUp from "../pages/SignUp";
+import AuthProvider from "../providers/authProvider";
 
 const Router = () => {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/signUp">
-          <SignUp />
-        </Route>
-        <Route exact path="/login">
-          <Login />
-        </Route>
-      </Switch>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/signUp">
+            <SignUp />
+          </Route>
+          <Route exact path="/login">
+            <Login />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </AuthProvider>
   );
 };
 
