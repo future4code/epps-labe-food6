27c972a72706b4bceb0c57a3eb51593dd5cb6903
cptvictor95 <cpt.victor@hotmail.com>
2021-03-02 17:22:29 -0300

    footer created

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import { Flex } from "@chakra-ui/react";
+import React from "react";
+
+const Footer = () => {
+  return (
+    <Flex as="footer" h="10vh" w="100vw" direction="column" align="center">
+      footer
+    </Flex>
+  );
+};
+
+export default Footer;
