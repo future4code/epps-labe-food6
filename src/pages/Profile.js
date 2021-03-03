1599237c6eaff8d42e11a8838917dec64f22d84d
astrid <astridbemporad@gmail.com>
2021-03-03 18:32:29 -0300

    Create Order History Card

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -2,6 +2,7 @@ import { Flex } from "@chakra-ui/react";
 import React from "react";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import OrderHistoryCard from "../components/OrderHistoryCard";
 import useAuth from "../hooks/useAuth";
 
 const Profile = () => {
@@ -9,7 +10,7 @@ const Profile = () => {
   return (
     <Flex as="main" w="100vw" h="100vh" direction="column" align="center">
       <Header>Profile</Header>
-      <Flex as="section" h="100%" w="100%">
+      <Flex as="section" h="100%" w="100%" flexDirection="column">
         profile page
       </Flex>
       <Footer />
