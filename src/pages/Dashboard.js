2aedfe0c5306c8eb802ce5e336e7dd6631735748
Bruno <silva.bruno@outlook.com>
2021-03-04 11:46:13 -0300

    Adicionado a rolagem no menu de categorias

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -16,34 +16,39 @@ import useAuth from "../hooks/useAuth";
 import RestaurantContext from "../contexts/restaurantContext";
 
 import { useInput } from "../hooks/useInput";
+import styled from 'styled-components'
+
+// const CategoryNav = styled(TabList)`
+
+// `
 
 const Dashboard = () => {
   const { states } = useContext(RestaurantContext);
   useAuth();
-  
+
   const [search, handleSearch] = useInput()
 
-//   const category = arrFood.reduce((acc, current) => {
-//     const key = acc.find(item => item.category === current.category);
-//     if (!key) {
-//       return acc.concat([current])
-//     } else {
-//       return acc
-//     }
-//   }, []).map((restaurant) => {
-//     return (
-//       <Tab key={restaurant.id}>{restaurant.category}</Tab>
-//     )
-//   })
-  const category = states.restaurants.map((type, index) => {
-    if (index < 4) return <Tab key={type.id}>{type.category}</Tab>;
-  });
+  const category = states.restaurants.reduce((acc, current) => {
+    const key = acc.find(item => item.category === current.category);
+    if (!key) {
+      return acc.concat([current])
+    } else {
+      return acc
+    }
+  }, []).map((restaurant) => {
+    return (
+      <Tab key={restaurant.id}>{restaurant.category}</Tab>
+    )
+  })
+  // const category = states.restaurants.map((type, index) => {
+  //   if (index < 4) return <Tab key={type.id}>{type.category}</Tab>;
+  // });
 
   const restaurantsList = states.restaurants.map((restaurant) => {
     // .filter => restaurante por tipo ===== array restaurantes
 
     // .filter => restaurante por nome ===== array restaurantes com nome >por tipo ou não<
-    console.log(restaurant.id);
+    // console.log(restaurant.id);
     return (
       <RestaurantCard
         key={restaurant.id}
@@ -70,22 +75,22 @@ const Dashboard = () => {
       >
         <Header>FutureEats</Header>
 
-          <Box paddingX="4" w="100%">
+        <Box paddingX="4" w="100%">
           <InputGroup marginTop="2">
-          <InputLeftElement
-            pointerEvents="none"
-            children={<Search2Icon color="gray.300" />}
-          />
-          <Input
-            value={search}
-            onChange={handleSearch}
-            placeholder="Restaurante"
-          />
-        </InputGroup>
+            <InputLeftElement
+              pointerEvents="none"
+              children={<Search2Icon color="gray.300" />}
+            />
+            <Input
+              value={search}
+              onChange={handleSearch}
+              placeholder="Restaurante"
+            />
+          </InputGroup>
         </Box>
 
-        <Tabs>
-          <TabList>{category && category}</TabList>
+        <Tabs maxW="100vw">
+          <TabList overflowX="auto">{category && category}</TabList>
           <TabPanels>{restaurantsList && restaurantsList}</TabPanels>
         </Tabs>
       </Flex>
